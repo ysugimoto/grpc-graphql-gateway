3d150297bcf58f1aa75fc7558341ea4c672d8d91
ysugimoto <sugimoto@wnotes.net>
2020-06-23 21:56:23 +0900

    Change middleware interface

--- runtime/middlewares.go
+++ runtime/middlewares.go
@@ -1,16 +1,17 @@
 package runtime
 
 import (
+	"context"
 	"net/http"
 )
 
 // Cors is middelware function to provide CORS headers to response headers
 func Cors() MiddlewareFunc {
-	return func(w http.ResponseWriter, r *http.Request) error {
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) (context.Context, error) {
 		w.Header().Set("Access-Control-Allow-Origin", r.URL.Host)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Max-Age", "1728000")
-		return nil
+		return ctx, nil
 	}
 }
