a22c2d191c12dd5669530d2ac933867d8fd774ac
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-07-25 20:27:18 -0500

    refactor package name retrieval

--- protoc-gen-graphql/spec/package.go
+++ protoc-gen-graphql/spec/package.go
@@ -26,26 +26,18 @@ type Package struct {
 
 func NewPackage(g PackageGetter) *Package {
 	p := &Package{}
+	p.Name = strings.TrimSuffix(filepath.Base(g.Filename()), filepath.Ext(g.Filename()))
+	p.GeneratedFilenamePrefix = strings.TrimSuffix(g.Filename(), filepath.Ext(g.Filename()))
+
 	if pkg := g.GoPackage(); pkg != "" {
 		// Support custom package definitions like example.com/path/to/package:packageName
 		if index := strings.Index(pkg, ";"); index > -1 {
-			p.Name = pkg[index+1:]
 			p.Path = pkg[0:index]
 		} else {
-			p.Name = filepath.Base(pkg)
 			p.Path = pkg
 		}
-	} else if pkg := g.Package(); pkg != "" {
-		p.Name = pkg
-	} else {
-		p.Name = strings.ReplaceAll(
-			g.Filename(),
-			filepath.Ext(g.Filename()),
-			"",
-		)
 	}
 
-	p.GeneratedFilenamePrefix = strings.TrimSuffix(g.Filename(), ".proto")
 	p.CamelName = strcase.ToCamel(p.Name)
 	return p
 }
